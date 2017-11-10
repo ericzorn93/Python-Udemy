f275d060dafa6db3da6f632912dd1bb3d4cb7df4
Eric Zorn <goldencatch93@gmail.com>
2017-11-09 23:37:11 -0500

    Arithmatic Operators and String Splicing

--- HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld.py
@@ -1,9 +1,39 @@
-print("Hello World!")
+#HelloWorld
+# print("Hello World!")
 
 #Initialize Variables
-name = "Eric Zorn"
-age = 22
+# name = "Eric Zorn"
+# age = 22
+#
+# firstName, lastName, ageSecond = "Eric", "Zorn", 22
+#
+# print(firstName, lastName, ageSecond)
 
-firstName, lastName, ageSecond = "Eric", "Zorn", 22
 
-print(firstName, lastName, ageSecond)
+
+#Arithmatic Operators
+age1 = 12
+age2 = 18
+
+print(age1 + age2)
+print(age1 - age2)
+print(age1 * age2)
+print(age1 / age2)
+print(age2 % age1)
+print(age1 % age2)
+
+sent1 = "Today was a beautiful day"
+print(sent1)
+
+firstName = "Eric"
+lastName = "Zorn"
+
+print(firstName + " " + lastName)
+
+print("Hi" * 10)
+
+
+sentence = "Eric Zorn was playing basketball"
+print(sentence[0])
+print(sentence[3])
+print(sentence[0:4])
